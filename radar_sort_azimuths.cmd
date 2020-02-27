5c2acb53ed72418e19501d996fca8119fabb9478
dmhuehol <dmhuehol@ncsu.edu>
2020-02-27 15:25:16 -0500

    Correct comments
    Some comments in radar_sort_azimuth had not been updated after being copied/pasted from radar_sweeps

--- radar_sort_azimuths.cmd
+++ radar_sort_azimuths.cmd
@@ -1,11 +1,13 @@
-:: Creates folders for individual azimuths and move radar images into the appropriate directories
+:: Creates folders for individual azimuths given as a space-separated list and
+::  moves radar images into the appropriate directories
 :: Written by Daniel Hueholt
 :: Undergraduate Research Assistant at Environment Analytics
 :: February 2020
 
 @ECHO OFF
 
-:: Prepare to count through the list of sweeps. User must input the start and end number of sweeps.
+:: Request the list of azimuths
+:: e.g. for a folder with azimuths 141, 168, 299, user would input 141 168 299
 set /p azimuths="Enter azimuth numbers as space-separated list: "
 
 :: Make folders
