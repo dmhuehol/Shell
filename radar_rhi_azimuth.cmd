b1a108b1ea0979b2f157af88f3c6d1b68b157080
dmhuehol <dmhuehol@ncsu.edu>
2020-03-19 12:29:28 -0400

    Add in-progress azimuth sorter

---
+++ radar_rhi_azimuth.cmd
@@ -0,0 +1,33 @@
+:: Creates folders for individual sweeps and move radar images into the appropriate directories
+:: Written by Daniel Hueholt
+:: Undergraduate Research Assistant at Environment Analytics
+:: October 2018
+
+@ECHO OFF
+
+:: Prepare to count through the list of sweeps. User must input the start and end number of sweeps.
+set /p start="Enter first sweep number: "
+set /p end="Enter final sweep number: "
+
+set /p scanType="PPI or RHI? "
+
+IF /I "%scanType%"=="PPI" (
+:: Make folders
+    FOR /L %%A IN (%start%,1,%end%) DO mkdir "Sweep %%A"
+
+:: Move appropriate images to the new folders
+    FOR /L %%A IN (%start%,1,%end%) DO move *.%%A.ang* "Sweep %%A"
+
+    ECHO.Completed!
+)
+
+IF /I "%scanType%"=="RHI" (
+:: Make folders
+    FOR /L %%A IN (%start%,1,%end%) DO mkdir "Sweep %%A"
+
+:: Move appropriate images to the new folders
+    FOR /L %%A IN (%start%,1,%end%) DO move *.%%A.* "Sweep %%A"
+
+    ECHO.Completed!
+)
+ECHO ON
