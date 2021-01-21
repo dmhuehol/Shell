b38ebe14f4f1faf0f87907c0c4d4b935707cdab3
Daniel Hueholt <dmhuehol@ncsu.edu>
2021-01-21 13:06:53 -0700

    Add prompt for filename in png_knit_mp4

--- png_knit_mp4.sh
+++ png_knit_mp4.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Knit a folder of png files into a single mp4
-ffmpeg -framerate 1 -pattern_type glob -i "*.png" -vcodec libx264 -s 1280x720 -pix_fmt yuv420p vidName.mp4
+read -p 'Enter output filename: ' vidName
+
+ffmpeg -framerate 1 -pattern_type glob -i "*.png" -vcodec libx264 -s 1280x720 -pix_fmt yuv420p $vidName
